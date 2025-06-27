485795a5e02d2dd555919c85db6576cb77804800
Daniela Soares <112218335+DanyelaSoares@users.noreply.github.com>
2025-06-27 18:30:05 -0300

    Update and rename index.go to desafioGo.go

---
+++ desafioGo.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var kelvin float64 = 373
+	var celsius float64
+
+	celsius = kelvin - 273
+
+	fmt.Println("A temperatura de ebulição da água em Celsius é =", celsius)
+}
--- index.go
+++
@@ -1,7 +0,0 @@
-/*criar um programa na linguagem GO que faça a conversao de escalas termometricas.*/
-
-package main
-
-func main() {
-
-}
